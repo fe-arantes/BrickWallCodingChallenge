8b8d2dab17c7504646dbf50b1bd3dc17100c5962
Felipe Arantes <fe.arantes@outlook.com>
2020-09-15 10:58:59 -0300

    Complexidade de Tempo

--- BrickWallCodingChallenge/BrickWall.Library/BrickWall.cs
+++ BrickWallCodingChallenge/BrickWall.Library/BrickWall.cs
@@ -8,8 +8,30 @@ namespace BrickWall.Library
     {
         public static int LeastBricks(int[][] brickWall)
         {
-            //Complexidade de Tempo = O(n)
-            //Cada vez que n dobra de tamanho, o tempo de execução dobra.
+            /*
+            Complexidade de Tempo = O(n)
+            
+            Explicação:
+            Sendo:
+                N = O número de tijolos em cada linha está em um range de [1,10.000]
+                M = A altura da parede está em um range de [1, 10.000]
+                TOTAL = O número total de tijolos não excederá 20.000, 
+                        logo, não existe o cenário de multiplar o máximo de tijolos na linha pelo máximo de tijolos na altura
+
+            Função:
+                F(x) = N * M <= 20.000
+
+            E:
+                Estou removendo o último tijolo, pois não podemos cortar nas extremidades
+
+            Logo a Função será:
+                F(x) = (N - 1) * M
+
+            sendo assim, podemos concluir que o resultado será de:
+                F(x) = N
+                
+             */
+
 
             var maxEdges = 0;
             var edgesPerBricksSum = new Dictionary<int, int>();
